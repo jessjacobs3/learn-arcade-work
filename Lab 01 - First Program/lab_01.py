50b03c8b40ecce5dbade90d34d409619c0ecf115
Jessica Jacobs <jessica.jacobs@my.simpson.edu>
2021-09-02 15:26:24 -0500

    Work on Lab 1

---
+++ Lab 01 - First Program/lab_01.py
@@ -0,0 +1,9 @@
+print("Hi, my name is Jessica.")
+print("I am majoring in Elementary Education.")
+print("My mom has always said, \"Never let the fear of striking out keep you from playing the game.\"")
+print("""I play volleyball 
+here at 
+Simpson College and 
+I enjoy it
+very much.""")
+
