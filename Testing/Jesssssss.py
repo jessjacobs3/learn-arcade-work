7a706e8a941a6fd45f329674b9191c6dc8716cf9
Jessica Jacobs <jessica.jacobs@my.simpson.edu>
2021-11-11 13:08:11 -0600

    Work on Lab 9

--- Testing/Jesssssss.py
+++ Testing/Jesssssss.py
@@ -3,48 +3,13 @@ import random
 
 # --- Constants ---
 SPRITE_SCALING_BOX = 1
-SPRITE_SCALING_PLAYER = 0.3
+SPRITE_SCALING_PLAYER = 0.4
 MOVEMENT_SPEED = 5
-PLAYER_SCALE = 0.4
+PLAYER_SCALE = 0.7
 COIN_SCALE = 0.35
-COIN_COUNT = 50
+COIN_COUNT = 30
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
-RIGHT_FACING = 1
-LEFT_FACING = 0
-CAMERA_SPEED = 1
-BOTTOM_Y = -420
-TOP_Y = 500
-LEFT_X = 0
-RIGHT_X = 1600
-
-
-class PlayerCharacter(arcade.Sprite):
-
-    def __init__(self):
-        # Set up parent class
-        super().__init__(hit_box_algorithm='Simple')
-
-        self.scale = SPRITE_SCALING_PLAYER
-        self.textures = []
-
-        # Load a left facing texture and a right facing texture.
-        # flipped_horizontally=True will mirror the image we load.
-        texture = arcade.load_texture("fishPink.png")
-        self.textures.append(texture)
-        texture = arcade.load_texture("fishPink.png",
-                                      flipped_horizontally=True)
-        self.textures.append(texture)
-
-        self.texture = self.textures[RIGHT_FACING]
-
-    def update(self):
-
-        # Figure out if we should face left or right
-        if self.change_x < 0:
-            self.texture = self.textures[LEFT_FACING]
-        elif self.change_x > 0:
-            self.texture = self.textures[RIGHT_FACING]
 
 
 class Coin(arcade.Sprite):
@@ -73,7 +38,6 @@ class Coin(arcade.Sprite):
         if self.top > SCREEN_HEIGHT:
             self.change_y *= -1
 
-
 class MyGame(arcade.Window):
     """ This class represents the main window of the game. """
 
@@ -86,17 +50,12 @@ class MyGame(arcade.Window):
         # Sprite Lists
         self.player_list = None
         self.wall_list = None
-        self.coin_list = None
-        self.good_hit_sound = arcade.sound.load_sound(":resources:sounds/coin1.wav")
 
         # Set up the player
         self.player_sprite = None
 
         self.camera_for_sprites = arcade.Camera(SCREEN_WIDTH, SCREEN_HEIGHT)
         self.camera_for_gui = arcade.Camera(SCREEN_WIDTH, SCREEN_HEIGHT)
-
-        # Score
-        self.score = 0
 
     def setup(self):
         # Set the background color
@@ -105,15 +64,25 @@ class MyGame(arcade.Window):
         # Sprite Lists
         self.player_list = arcade.SpriteList()
         self.wall_list = arcade.SpriteList()
-        self.coin_list = arcade.SpriteList()
+
+        # Score
+        self.score = 0
 
         # Set up player
-        self.player_sprite = PlayerCharacter()
+        self.player_sprite = arcade.Sprite("fishPink.png", PLAYER_SCALE, flipped_horizontally=True)
         self.player_sprite.center_x = 50
         self.player_sprite.center_y = 50
         self.player_list.append(self.player_sprite)
 
-        # Place Walls Outside
+        # Create the Coins
+        for i in range(COIN_COUNT):
+            coin = Coin("coinGold_ul.png", COIN_SCALE)
+
+            coin.center_x = random.randrange(SCREEN_WIDTH)
+            coin.center_y = random.randrange(120, SCREEN_HEIGHT)
+            coin.change_x = random.randrange(-3, 4)
+            coin.change_y = random.randrange(-3, 4)
+
         for x in range(0, SCREEN_WIDTH * 2, 45):
             wall = arcade.Sprite("button_square_blue_pressed.png", SPRITE_SCALING_BOX)
             wall.center_x = x
@@ -221,34 +190,6 @@ class MyGame(arcade.Window):
                 wall.center_y = 275
                 self.wall_list.append(wall)
 
-        self.physics_engine = arcade.PhysicsEngineSimple(self.player_sprite, self.wall_list)
-
-        # Create the Coins
-        for i in range(COIN_COUNT):
-            # Coin image from api.arcade.academy in Python Arcade Library Resources
-            coin = Coin("coinGold_ul.png", COIN_SCALE)
-
-            coin_placed_successfully = False
-
-            # Keep trying until success
-            while not coin_placed_successfully:
-                # Position the coin
-                coin.center_x = random.randrange(LEFT_X, RIGHT_X)
-                coin.center_y = random.randrange(BOTTOM_Y, TOP_Y)
-
-                # See if the coin is hitting a wall
-                wall_hit_list = arcade.check_for_collision_with_list(coin, self.wall_list)
-
-                # See if the coin is hitting another coin
-                coin_hit_list = arcade.check_for_collision_with_list(coin, self.coin_list)
-
-                if len(wall_hit_list) == 0 and len(coin_hit_list) == 0:
-                    # It is!
-                    coin_placed_successfully = True
-
-            # Add the coin to the lists
-            self.coin_list.append(coin)
-
     def on_draw(self):
         arcade.start_render()
 
@@ -256,33 +197,19 @@ class MyGame(arcade.Window):
 
         self.player_list.draw()
         self.wall_list.draw()
-        self.coin_list.draw()
 
         self.camera_for_gui.use()
-
-        if self.score == COIN_COUNT:
-            arcade.draw_text("You Win!",
-                             SCREEN_WIDTH / 2,
-                             SCREEN_HEIGHT / 2,
-                             arcade.color.WHITE, 80,
-                             anchor_x="center")
+        arcade.draw_text(f"Score: {self.score}", 10, 10, arcade.color.WHITE, 24)
 
     def update(self, delta_time):
         self.player_sprite.update()
         self.physics_engine.update()
         if len(self.coin_list) != 0:
             self.coin_list.update()
-
+        CAMERA_SPEED = 1
         lower_left_corner = (self.player_sprite.center_x - self.width / 2,
                              self.player_sprite.center_y - self.height / 2)
         self.camera_for_sprites.move_to(lower_left_corner, CAMERA_SPEED)
-
-        good_hit_list = arcade.check_for_collision_with_list(self.player_sprite, self.coin_list)
-
-        for coin in good_hit_list:
-            coin.remove_from_sprite_lists()
-            self.score += 1
-            arcade.play_sound(self.good_hit_sound)
 
     def on_key_press(self, key, modifiers):
         """Called whenever a key is pressed. """
