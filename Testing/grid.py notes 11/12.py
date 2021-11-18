3f8ab27fabaa843d2acbb01580aca111d0d7edbc
Jessica Jacobs <jessica.jacobs@my.simpson.edu>
2021-11-18 16:07:17 -0600

    Work on Lab 11 Part 1

---
+++ Testing/grid.py notes 11/12.py
@@ -0,0 +1,78 @@
+import arcade
+import random
+
+WIDTH = 60
+HEIGHT = 60
+MARGIN = 5
+COLUMN_COUNT = 10
+ROW_COUNT = 10
+
+SCREEN_WIDTH = (WIDTH + MARGIN) * COLUMN_COUNT + MARGIN
+SCREEN_HEIGHT = (HEIGHT + MARGIN) * ROW_COUNT + MARGIN
+
+
+class MyGame(arcade.Window):
+    """
+    Main application class.
+    """
+
+    def __init__(self, width, height):
+        super().__init__(width, height)
+
+        arcade.set_background_color(arcade.color.BLACK)
+
+        # Create grid of numbers
+        self.grid = []
+        for row in range(ROW_COUNT):
+            self.grid.append([])
+            for column in range(COLUMN_COUNT):
+                self.grid[row].append(0)
+
+        self.grid[2][1] = 1
+
+        print(self.grid)
+
+    def on_draw(self):
+        """
+        Render the screen.
+        """
+
+        arcade.start_render()
+        for row in range(ROW_COUNT):
+            for column in range(COLUMN_COUNT):
+                x = WIDTH / 2 + column * (WIDTH + MARGIN) + MARGIN
+                y = HEIGHT / 2 + row * (HEIGHT + MARGIN) + MARGIN
+                color = arcade.color.WHITE
+                if self.grid[row][column] == 0:
+                    color = (random.randrange(256), random.randrange(256), random.randrange(256))
+                else:
+                    color = arcade.color.GREEN
+
+                arcade.draw_rectangle_filled(x, y,
+                                             WIDTH, HEIGHT,
+                                             color)
+
+    def on_mouse_press(self, x, y, button, key_modifiers):
+        """
+        Called when the user presses a mouse button.
+        """
+
+        row = y // (HEIGHT + MARGIN)
+        column = x // (WIDTH + MARGIN)
+
+        if self.grid[row][column] == 0:
+            self.grid[row][column] = 1
+        else:
+            self.grid[row][column] = 0
+
+        print("Click", row, column)
+
+
+def main():
+
+    window = MyGame(SCREEN_WIDTH, SCREEN_HEIGHT)
+    arcade.run()
+
+
+if __name__ == "__main__":
+    main()
